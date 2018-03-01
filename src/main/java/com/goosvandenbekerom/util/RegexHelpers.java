e2dfda64d59c185ddc65e96dc56659a34d943238
Goos <goos.bekerom@gmail.com>
2018-03-01 14:36:49 +0100

    Changed url regex

--- src/main/java/com/goosvandenbekerom/util/RegexHelpers.java
+++ src/main/java/com/goosvandenbekerom/util/RegexHelpers.java
@@ -3,5 +3,5 @@ package com.goosvandenbekerom.util;
 public class RegexHelpers {
     public static final String HASHTAG = "(?<=#)(\\S+)";
     public static final String MENTION = "(?<=@)(\\S+)";
-    public static final String URL = "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]\\.[^\\s]{2,})";
+    public static final String URL = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$";
 }
