bdf884a39938f7c902e1d054cb0a88da11537d3f
Goos <goos.bekerom@gmail.com>
2018-03-07 10:33:32 +0100

    Added info to api definition

--- src/main/java/com/goosvandenbekerom/Resource/package-info.java
+++ src/main/java/com/goosvandenbekerom/Resource/package-info.java
@@ -1,4 +1,9 @@
 @OpenAPIDefinition(
+    info = @Info(
+        title = "Kwetter API",
+        version = "1.0.-SNAPSHOT",
+        description = "API to communicate with Kwetter backend"
+    ),
     servers = {
         @Server(description = "local", url = "http://localhost:8080/kwetter-java-1.0-SNAPSHOT/api")
     }
@@ -6,4 +11,5 @@
 package com.goosvandenbekerom.Resource;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
