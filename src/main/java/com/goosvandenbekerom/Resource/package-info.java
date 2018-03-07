c1a43b74ad6433ffc7a380bcb5383df74f5b70cc
Goos <goos.bekerom@gmail.com>
2018-03-07 10:23:35 +0100

    Added server specification for resource package for swagger ui

---
+++ src/main/java/com/goosvandenbekerom/Resource/package-info.java
@@ -0,0 +1,9 @@
+@OpenAPIDefinition(
+    servers = {
+        @Server(description = "local", url = "http://localhost:8080/kwetter-java-1.0-SNAPSHOT/api")
+    }
+)
+package com.goosvandenbekerom.Resource;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
