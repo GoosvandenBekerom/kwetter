205069c446cfead75cbeb784def2ef751f411915
Goos <goos.bekerom@gmail.com>
2018-04-05 19:15:11 +0200

    Extracted kweet form to component

---
+++ angular/src/app/modules/kweet-form/kweet-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kweet-form',
+  templateUrl: './kweet-form.component.html',
+  styleUrls: ['./kweet-form.component.css']
+})
+export class KweetFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
