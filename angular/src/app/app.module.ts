3ecad6b99ac09d471c03a6ba14cf8fe11ae1173f
Goos <goos.bekerom@gmail.com>
2018-04-05 16:08:04 +0200

    Added home module and router

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './modules/navbar/navbar.component';
+import { HomeComponent } from './modules/home/home.component';
 
+const routes: Routes = [
+  {path:'', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
