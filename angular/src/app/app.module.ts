5021cbe2a95766b18b0226308ebf6083b00c0aec
Goos <goos.bekerom@gmail.com>
2018-04-09 19:35:22 +0200

    Added angular forms module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -13,7 +14,7 @@ import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent },
-  {path:'search', component: SearchComponent }
+  {path:'search/:query', component: SearchComponent }
 ];
 
 @NgModule({
@@ -29,7 +30,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
