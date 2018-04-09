76de059967e66763800fb2f6230ffbd6b805427b
Goos <goos.bekerom@gmail.com>
2018-04-09 13:29:09 +0200

    Added kweet component and rename modules folder to components

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './modules/navbar/navbar.component';
-import { HomeComponent } from './modules/home/home.component';
-import { UserCardComponent } from './modules/user-card/user-card.component';
-import { TrendingCardComponent } from './modules/trending-card/trending-card.component';
-import { KweetFormComponent } from './modules/kweet-form/kweet-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { TrendingCardComponent } from './components/trending-card/trending-card.component';
+import { KweetFormComponent } from './components/kweet-form/kweet-form.component';
+import { KweetComponent } from './components/kweet/kweet.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent }
@@ -20,7 +21,8 @@ const routes: Routes = [
     HomeComponent,
     UserCardComponent,
     TrendingCardComponent,
-    KweetFormComponent
+    KweetFormComponent,
+    KweetComponent
   ],
   imports: [
     BrowserModule,
