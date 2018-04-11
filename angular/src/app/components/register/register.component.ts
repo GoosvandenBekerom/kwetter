693375058631e65ff6079a4708f6df7dae84f2bc
Goos <goos.bekerom@gmail.com>
2018-04-11 02:27:40 +0200

    Fixed register functionality

--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FlashService} from "../../services/flash.service";
 import {Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import {UserService} from "../../services/user.service";
 export class RegisterComponent implements OnInit {
   username: string
   password: string
+  password_again: string
   fullName: string
 
   constructor(
@@ -21,6 +23,21 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {}
 
   onRegisterSubmit() {
-
+    if (this.username == undefined || this.password == undefined || this.fullName == undefined) {
+      this.flash.error('Make sure you fill out all fields')
+      return
+    }
+    if (this.password !== this.password_again) {
+      this.flash.error('The passwords you entered do not match')
+      return
+    }
+    this.userService.register(this.username, this.password, this.fullName)
+      .subscribe(
+        () => {
+          this.flash.success('Successfully registered!')
+          this.router.navigate(['/login'])
+        },
+        (err: HttpErrorResponse) => this.flash.error(err.error.message)
+      )
   }
 }
--- angular/src/app/services/user.service.ts
+++ angular/src/app/services/user.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {BASE_URL, getHeaders} from "../utils/constants";
 
 @Injectable()
 export class UserService {
   constructor(private http: HttpClient) { }
 
-  private static getHeaders() {
-    let headers = new HttpHeaders()
-    headers.append('Content-Type', 'application/x-www-form-urlencoded')
-    return headers
+  register(username: string, password: string, fullName: string) {
+    const body = new HttpParams().set("username", username).set("password", password).set("fullName", fullName)
+    return this.http.post(`${BASE_URL}/user`, body, {headers: getHeaders(), observe: "response"})
   }
 }
