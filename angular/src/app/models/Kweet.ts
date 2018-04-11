b74e0245536a12fbd3c7f8917ebf794738d92ddc
Goos <goos.bekerom@gmail.com>
2018-04-11 23:14:02 +0200

    Updated models so dates are saved as strings

--- angular/src/app/models/Kweet.ts
+++ angular/src/app/models/Kweet.ts
@@ -1,12 +1,5 @@
 import {User} from "./User";
 
 export class Kweet {
-  constructor(public id: number, public owner: User, public message: string, public likeCount: number, public created: Date) {}
-
-  static from(x: any) {
-    if (x.id == undefined || x.owner == undefined || x.message == undefined || x.likeCount == undefined
-      || x.created == undefined) return null
-
-    return new Kweet(x.id, x.owner, x.message, x.likeCount, x.created)
-  }
+  constructor(public id: number, public owner: User, public message: string, public likeCount: number, public created: string) {}
 }
--- angular/src/app/models/User.ts
+++ angular/src/app/models/User.ts
@@ -1,9 +1,10 @@
 export class User{
-  constructor(public username: string, public fullName: string, public created: Date) {}
-
-  static from(object: any) : User {
-    if (object.username == undefined || object.fullName == undefined || object.created == undefined) return null
-
-    return new User(object.username, object.fullName, object.created)
-  }
+  constructor(
+    public username: string,
+    public fullName: string,
+    public kweetCount: number,
+    public followerCount: number,
+    public followingCount: number,
+    public created: string
+  ) {}
 }
