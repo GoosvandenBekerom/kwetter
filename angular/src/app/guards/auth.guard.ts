87246921f76e0c6a9160f5ca294f6451fa603900
Goos <goos.bekerom@gmail.com>
2018-04-12 12:50:36 +0200

    removed unused authservice injection

--- angular/src/app/guards/auth.guard.ts
+++ angular/src/app/guards/auth.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from "../services/auth.service";
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router) {}
 
   canActivate() {
     if (!AuthService.isTokenExpired()) {
